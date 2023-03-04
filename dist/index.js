e214354cbd9941af1d0efa502f994ef82e763171
Capopato <tommpato@gmail.com>
2023-03-04 14:07:56 +0100

    second add

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,28 @@
 "use strict";
-/**
- * Quiz app
- *
- * Action plan:
- * Make a file/function that fetches questions from an api
- * console.log the question and ask the user for input
- * check if input is the correct answer
- * go to next question
- *
- * When making the API call the response is X amount of question + answers
- * Action plan:
- * make a class
- * make a function for the API call or import it
- * make a [] for storing the API call data
- * make a function that only displays the question + all the available answers.
- * make a prompt-sync function to ask user for input to choose from 1 of the answers
- * compare the input to the correct answer
- * score point if won
- *
- * make while loop to iterate over all the question. When all the questions are done ask if you want to play another round to get new question.
- * To proceed do the following: make 1 API call and store the data in a []
- *
- *
- */
+// /**
+//  * Quiz app
+//  *
+//  * Action plan:
+//  * Make a file/function that fetches questions from an api
+//  * console.log the question and ask the user for input
+//  * check if input is the correct answer
+//  * go to next question
+//  *
+//  * When making the API call the response is X amount of question + answers
+//  * Action plan:
+//  * make a class
+//  * make a function for the API call or import it
+//  * make a [] for storing the API call data
+//  * make a function that only displays the question + all the available answers.
+//  * make a prompt-sync function to ask user for input to choose from 1 of the answers
+//  * compare the input to the correct answer
+//  * score point if won
+//  *
+//  * make while loop to iterate over all the question. When all the questions are done ask if you want to play another round to get new question.
+//  * To proceed do the following: make 1 API call and store the data in a []
+//  *
+//  *
+//  */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -38,88 +38,59 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Quiz = void 0;
 const axios_1 = __importDefault(require("axios"));
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const prompt = (0, prompt_sync_1.default)();
 class Quiz {
     constructor() {
         this.questionData = [];
-        this.onlyQuestions = [];
-        this.onlyAnswers = [];
-        this.correctAnswer = [];
+        this.running = true;
         this.round = 0;
-        this.running = false;
+        this.readyToAnswer = false;
+        this.scoreCorrect = 0;
+        this.scoreInCorrect = 0;
+        this.playRound = true;
     }
     getData() {
         return __awaiter(this, void 0, void 0, function* () {
             const url = "https://the-trivia-api.com/api/questions";
             const response = yield axios_1.default.get(url);
             const relevantData = response.data;
-            if (!relevantData) {
-                console.log("Something went wrong. Please try again.");
-                return;
-            }
             this.questionData.push(...relevantData);
-            /** Get only the questions and push them to a seperate [] */
-            for (let i = 0; i < this.questionData.length; i++) {
-                this.onlyQuestions.push(this.questionData[i].question);
-            }
-            /** Get only the answers and push them to a seperate [] */
-            for (let i = 0; i < this.questionData.length; i++) {
-                //   console.log(this.questionData[i]);
-                this.onlyAnswers.push([this.questionData[i].correctAnswer + "," + this.questionData[i].incorrectAnswers]);
-            }
-            /** Get only the correct answer and push it to a seperate [] */
-            for (let i = 0; i < this.questionData.length; i++) {
-                //   console.log(this.questionData[i]);
-                this.correctAnswer.push(this.questionData[i].correctAnswer);
-            }
         });
     }
-    displayQuestion() {
+    runQuiz() {
         return __awaiter(this, void 0, void 0, function* () {
             yield this.getData();
-            console.log(this.onlyQuestions[this.round]);
+            const question = this.questionData[this.round].question;
+            const possibleAnswers = this.questionData[this.round].incorrectAnswers + "," + this.questionData[this.round].correctAnswer;
+            const correctAnswers = this.questionData[this.round].correctAnswer;
+            console.log("\n");
+            console.log(question);
+            console.log(possibleAnswers);
+            console.log("\n");
+            const answer = prompt("Answer: ");
+            if (answer == correctAnswers) {
+                this.scoreCorrect += 1;
+                console.log("That is correct!");
+            }
+            else {
+                this.scoreInCorrect += 1;
+                console.log("That is incorrect!");
+            }
             this.round += 1;
-        });
-    }
-    displayAnswers() {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield this.getData();
-            console.log(this.onlyAnswers[this.round]);
-            this.round += 1;
-        });
-    }
-    checkAnswer() {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield this.getData();
+            if (this.round == 18) {
+                console.log(`You have ${this.scoreCorrect} questions right and ${this.scoreInCorrect} questions wrong.`);
+            }
         });
     }
 }
 exports.Quiz = Quiz;
-const quiz = new Quiz();
-// quiz.checkAnswer();
-while (quiz.round <= 2) {
-    quiz.running = false;
-    while (quiz.running == false) {
-        quiz.displayQuestion();
-        quiz.displayAnswers();
-        // ask input
-        quiz.running = true;
+const game = new Quiz();
+for (let i = 0; i < 9; i++) {
+    game.playRound = true;
+    while (game.playRound == true) {
+        game.runQuiz();
+        game.playRound = false;
+        game.round += 1;
     }
-    quiz.round += 1;
 }
-/**
- * while(quiz.running = false){
- * quiz.getData()
- *  while(quiz.round = false){
- *      display question: quiz.onlyquestion[round]
- *      ask input: prompt(asnwer)
- *      check if answer is correct with a method
- *      quiz.round = true
- *  }
- *
- *  quiz.round += 1
- *
- *  if(quiz.round == 10){
- *      quiz.running = true
- *  }
- * }
- */
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,50 @@
-/**
- * Quiz app
- *
- * Action plan:
- * Make a file/function that fetches questions from an api
- * console.log the question and ask the user for input
- * check if input is the correct answer
- * go to next question
- *
- * When making the API call the response is X amount of question + answers
- * Action plan:
- * make a class
- * make a function for the API call or import it
- * make a [] for storing the API call data
- * make a function that only displays the question + all the available answers.
- * make a prompt-sync function to ask user for input to choose from 1 of the answers
- * compare the input to the correct answer
- * score point if won
- *
- * make while loop to iterate over all the question. When all the questions are done ask if you want to play another round to get new question.
- * To proceed do the following: make 1 API call and store the data in a []
- *
- *
- */
+// /**
+//  * Quiz app
+//  *
+//  * Action plan:
+//  * Make a file/function that fetches questions from an api
+//  * console.log the question and ask the user for input
+//  * check if input is the correct answer
+//  * go to next question
+//  *
+//  * When making the API call the response is X amount of question + answers
+//  * Action plan:
+//  * make a class
+//  * make a function for the API call or import it
+//  * make a [] for storing the API call data
+//  * make a function that only displays the question + all the available answers.
+//  * make a prompt-sync function to ask user for input to choose from 1 of the answers
+//  * compare the input to the correct answer
+//  * score point if won
+//  *
+//  * make while loop to iterate over all the question. When all the questions are done ask if you want to play another round to get new question.
+//  * To proceed do the following: make 1 API call and store the data in a []
+//  *
+//  *
+//  */
 
-import { triviaQuiz } from "./fetch/quiz.fetch";
 import { questionModel } from "./models/question.model";
 import axios from "axios";
+import promptSync from "prompt-sync";
+
+const prompt = promptSync();
 
 export class Quiz {
   questionData: questionModel[];
-  onlyQuestions: string[];
-  onlyAnswers: string[][];
-  correctAnswer: string[];
-  round: number;
   running: boolean;
+  round: number;
+  readyToAnswer: boolean;
+  scoreCorrect: number;
+  scoreInCorrect: number;
+  playRound: boolean;
   constructor() {
     this.questionData = [];
-    this.onlyQuestions = [];
-    this.onlyAnswers = [];
-    this.correctAnswer = [];
+    this.running = true;
     this.round = 0;
-    this.running = false;
+    this.readyToAnswer = false;
+    this.scoreCorrect = 0;
+    this.scoreInCorrect = 0;
+    this.playRound = true;
   }
 
   public async getData() {
@@ -48,78 +52,44 @@ export class Quiz {
     const response = await axios.get(url);
     const relevantData: questionModel[] = response.data;
 
-    if (!relevantData) {
-      console.log("Something went wrong. Please try again.");
-      return;
-    }
-
     this.questionData.push(...relevantData);
-
-    /** Get only the questions and push them to a seperate [] */
-    for (let i = 0; i < this.questionData.length; i++) {
-      this.onlyQuestions.push(this.questionData[i].question);
-    }
-
-    /** Get only the answers and push them to a seperate [] */
-    for (let i = 0; i < this.questionData.length; i++) {
-      //   console.log(this.questionData[i]);
-      this.onlyAnswers.push([this.questionData[i].correctAnswer + "," + this.questionData[i].incorrectAnswers]);
-    }
-
-    /** Get only the correct answer and push it to a seperate [] */
-    for (let i = 0; i < this.questionData.length; i++) {
-      //   console.log(this.questionData[i]);
-      this.correctAnswer.push(this.questionData[i].correctAnswer);
-    }
   }
 
-  public async displayQuestion() {
+  public async runQuiz() {
     await this.getData();
-    console.log(this.onlyQuestions[this.round]);
+    const question = this.questionData[this.round].question;
+    const possibleAnswers = this.questionData[this.round].incorrectAnswers + "," + this.questionData[this.round].correctAnswer;
+    const correctAnswers = this.questionData[this.round].correctAnswer;
+
+    console.log("\n");
+    console.log(question);
+    console.log(possibleAnswers);
+    console.log("\n");
+
+    const answer = prompt("Answer: ");
+
+    if (answer == correctAnswers) {
+      this.scoreCorrect += 1;
+      console.log("That is correct!");
+    } else {
+      this.scoreInCorrect += 1;
+      console.log("That is incorrect!");
+    }
     this.round += 1;
-  }
 
-  public async displayAnswers() {
-    await this.getData();
-    console.log(this.onlyAnswers[this.round]);
-    this.round += 1;
-  }
-
-  public async checkAnswer() {
-    await this.getData();
+    if (this.round == 18) {
+      console.log(`You have ${this.scoreCorrect} questions right and ${this.scoreInCorrect} questions wrong.`);
+    }
   }
 }
 
-const quiz = new Quiz();
-// quiz.checkAnswer();
+const game = new Quiz();
 
-while (quiz.round <= 2) {
-  quiz.running = false;
-
-  while (quiz.running == false) {
-    quiz.displayQuestion();
-    quiz.displayAnswers();
-    // ask input
-    quiz.running = true;
+for (let i = 0; i < 9; i++) {
+  game.playRound = true;
+  while (game.playRound == true) {
+    game.runQuiz();
+    game.playRound = false;
+    game.round += 1;
   }
-
-  quiz.round += 1;
 }
-
-/**
- * while(quiz.running = false){
- * quiz.getData()
- *  while(quiz.round = false){
- *      display question: quiz.onlyquestion[round]
- *      ask input: prompt(asnwer)
- *      check if answer is correct with a method
- *      quiz.round = true
- *  }
- *
- *  quiz.round += 1
- *
- *  if(quiz.round == 10){
- *      quiz.running = true
- *  }
- * }
- */
